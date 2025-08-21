377713fd4a858760a61727754dcb3b252eefbd27
Emily Ma <emilyma@outlook.com>
2025-08-21 12:10:16 -0700

    specifying port

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import torch
 from training.model import NeuralNetwork
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -61,4 +62,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
